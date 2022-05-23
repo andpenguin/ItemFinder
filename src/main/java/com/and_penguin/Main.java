c3189c2b6a25405ededa3bb8ef7aae33b00da603
and_penguin <24adams@students.harker.org>
2022-05-23 15:05:18 -0700

    Visual Updates

--- src/main/java/com/and_penguin/Main.java
+++ src/main/java/com/and_penguin/Main.java
@@ -25,25 +25,42 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+/**
+ * The Main class manages the searching in the background of the application.
+ * It saves an Item class from a list of all items, sends an enchantment list to the application,
+ * and then loops through a spiral pattern for the item.
+ *
+ * @author and_penguin
+ * @author libaries created by Neil helped simulate chest loot and structure positions
+ */
 public class Main {
-    // Looking through a seed randomly
-    private static Item item;
-    private static BPos itemLoc;
-    private static LinkedHashMap<Integer, Item> items;
-    private static final ChunkRand rand = new ChunkRand();
-    private static final MCVersion VERSION = MCVersion.v1_16_1;
+    private static Item item; // The item to be searched for
+    private static BPos itemLoc; // The closest location the item was found
+    private static LinkedHashMap<Integer, Item> items; // The list of all possible items
+    private static final ChunkRand rand = new ChunkRand(); // Used to simulate Java Random
+    private static final MCVersion VERSION = MCVersion.v1_16_1; // Version of the game to search on
+    // Different structures to search in
     private static final RuinedPortal portal = new RuinedPortal(Dimension.OVERWORLD, VERSION);
     private static final DesertPyramid temple = new DesertPyramid(VERSION);
     private static final BuriedTreasure treasure = new BuriedTreasure(VERSION);
     private static final Shipwreck shipwreck = new Shipwreck(VERSION);
     private static final EndCity city = new EndCity(VERSION);
 
+    /**
+     * Creates the application window and initializes the list of items
+     * @param args arguments from the command line
+     */
     public static void main (String[] args) {
         WindowFrame window = new WindowFrame();
         window.display();
         items = Items.getItems();
     }
 
+    /**
+     * Creates an array of all possible enchantment names to
+     * be used in the dropdown menu
+     * @return the list of possible enchant names
+     */
     public static String[] getEnchantNames() {
         List<Enchantment> enchantList = new ArrayList<>();
         Enchantments.apply(enchantList, VERSION);
@@ -54,8 +71,14 @@ public class Main {
         return nameList;
     }
 
+    /**
+     * Returns a list of enchantments (Pairs of String names and Integer levels)
+     * that match a certain string name and int level
+     * @param name The name of the enchantments to be found
+     * @param level the level of the enchantments to be found
+     * @return a list of enchantments (always size of 1) that match the parameters
+     */
     public static ArrayList<Pair<String,Integer>> getEnchantByName(String name, int level) {
-        System.out.println(level);
         List<Enchantment> enchantList = new ArrayList<>();
         Enchantments.apply(enchantList, VERSION);
         ArrayList<Pair<String,Integer>> enchants = new ArrayList<>();
@@ -69,41 +92,69 @@ public class Main {
         return null;
     }
 
-    public static Item getItemByName(String name) {
+    /**
+     * Initializes the item instance variable given an item name
+     *
+     * @param name the technical name for an item - eg: enchanted_golden_apple
+     * @return true if the name succesfully matched the name of an item, otherwise,
+     *         false
+     */
+    public static boolean setItemByName(String name) {
         for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).getName().equals(name))
+            if (items.get(i).getName().equals(name)) {
                 item = items.get(i);
+                return true;
+            }
         }
-        return null;
+        return false;
     }
 
+    /**
+     * Finds information about the location of the closest of an item on a given seed
+     * @param seed the seed to search for the item on
+     * @param enchants enchants to be added to the item
+     * @return Information about the seed and location of the item
+     */
     public static String findItemOnSeed(long seed, ArrayList<Pair<String, Integer>> enchants) {
-        System.out.println("Locating the item...");
-        item.setEnchantments(enchants);
-        BPos loc = findClosestItem(seed);
-        return "X: " + loc.getX() + " Z: " + loc.getZ();
+        item.setEnchantments(enchants); // Add the enchantments
+        findClosestItem(seed); // Find the item's location
+        if (itemLoc != null) { // If an item was found
+            int x = itemLoc.getX();
+            int z = itemLoc.getZ();
+            itemLoc = null; // Reset the item's position for future uses
+            return "Seed: " + seed + " - Location: X: " + x + " Z: " + z;
+        }
+        return "Seed: " + seed +  " - Could not find that item within a reasonable distance";
     }
 
-    public static BPos findClosestItem(long seed) {
+    /**
+     * Finds the actual location of the closest item on a given seed
+     * The method searches in a spiral pattern, checking for the item at each point on the spiral
+     * @param seed the seed to search on
+     * @return the Block position of the chest where the item is (accurate to a +- 8 blocks)
+     */
+    public static void findClosestItem(long seed) {
+        // Initial spiral position
         int x = -1;
         int z = -1;
         int steps = 0;
         int sideLength = 1;
         int position = 0;
         int turns = 0;
+        // Spiral directions
         String[] directions = new String[] {"right", "down", "left", "up"};
-        while (!findItem(seed, x, z)) {
-            if (steps == sideLength)
+        while (!findItem(seed, x, z)) { // While still searching
+            if (steps == sideLength) // If at a turn
             {
-                position++;
-                turns++;
-                if (position == directions.length)
+                position++; // change direction
+                turns++; // Add to turns counter
+                if (position == directions.length) // if you go over the array length
                     position = 0;
-                if (turns % 2 == 0)
+                if (turns % 2 == 0) // If side length needs to be extended
                     sideLength++;
-                steps = 0;
+                steps = 0; // Reset steps counter
             }
-            switch (directions[position]) {
+            switch (directions[position]) { // Find which direction to change
                 case "right": x++;
                     break;
                 case "down": z++;
@@ -114,30 +165,56 @@ public class Main {
                     break;
                 default: break;
             }
-            steps++;
+            steps++; // Increase steps counter
+            if (x >= 20 || z >= 20) // If item is unreasonably far out (likely not a real item)
+                return;
         }
-        return itemLoc;
     }
 
+    /**
+     * Finds if the item is in a given structure region
+     * @param seed the seed to search on
+     * @param x the x coordinate of the given structure region
+     * @param z the z coordinate of the given structure region
+     * @return true if the item is in any of the valid structures in the (x,z)
+     *         structure region, otherwise,
+     *         false
+     */
     public static boolean findItem(long seed, int x, int z) {
         boolean inPortal = false;
         boolean inTemple = false;
         boolean inTreasure = false;
         boolean inShip = false;
         boolean inCity = false;
+        //ruined portal check
         if (new RuinedPortalGenerator(VERSION).getPossibleLootItems().contains(item))
             inPortal  = findInStructure(portal, new RuinedPortalGenerator(VERSION), seed, x, z);
+        //desert temple check
         if (new DesertPyramidGenerator(VERSION).getPossibleLootItems().contains(item))
             inTemple = findInStructure(temple, new DesertPyramidGenerator(VERSION), seed, x, z);
+        // buried treasure check
         if (new BuriedTreasureGenerator(VERSION).getPossibleLootItems().contains(item))
             inTreasure = findInStructure(treasure, new BuriedTreasureGenerator(VERSION), seed, x, z);
+        // shipwreck check
         if (new ShipwreckGenerator(VERSION).getPossibleLootItems().contains(item))
             inShip = findInStructure(shipwreck, new ShipwreckGenerator(VERSION), seed, x, z);
+        // end city check
         if (new EndCityGenerator(VERSION).getPossibleLootItems().contains(item))
             inCity = findInEndCity(city, new EndCityGenerator(VERSION), seed, x, z);
+        // Check if any of the structures are true
         return inPortal || inTemple || inTreasure || inShip || inCity;
     }
 
+    /**
+     * Checks if the item can be found in a specific structure of a given region
+     * @param structure a subclass of the RegionStructure that can be checked for loot
+     * @param gen the Generator that matches the region structure
+     * @param seed the seed to search on
+     * @param x the x coordinate of the region
+     * @param z the z coordinate of teh region
+     * @return true if the structure contains the item, otherwise,
+     *         false
+     */
     public static boolean findInStructure(RegionStructure structure, Generator gen, long seed, int x, int z) {
         CPos structureLoc = structure.getInRegion(seed, x, z, rand);
         OverworldBiomeSource source = new OverworldBiomeSource(VERSION, seed);
@@ -150,7 +227,7 @@ public class Main {
         ILoot lootChecker = (ILoot) structure;
         List<ChestContent> loot = lootChecker.getLoot(seed, gen,false);
         for (ChestContent chest: loot)
-            if (contains(chest, item)) {
+            if (contains(chest)) {
                 if (itemLoc == null || itemLoc.getMagnitudeSq() > structureLoc.toBlockPos().getMagnitudeSq())
                     itemLoc = structureLoc.toBlockPos();
                 return true;
@@ -158,25 +235,42 @@ public class Main {
         return false;
     }
 
+    /**
+     * Seperate method similar to findInStructure for end cities
+     * because they are in a different dimension
+     * @param city the EndCity to check in
+     * @param gen the EndCity's generator to check for loot with
+     * @param seed the seed to check on
+     * @param x the x region to check in
+     * @param z the z region to check in
+     * @return
+     */
     public static boolean findInEndCity(EndCity city, EndCityGenerator gen, long seed, int x, int z) {
         CPos structureLoc = city.getInRegion(seed, x, z, rand);
-        EndBiomeSource source = new EndBiomeSource(VERSION, seed);
         if (structureLoc == null)
             return false;
+        EndBiomeSource source = new EndBiomeSource(VERSION, seed);
         if (!city.canSpawn(structureLoc, source))
             return false;
         if (!gen.generate(new EndTerrainGenerator(source), structureLoc))
             return false;
         List<ChestContent> loot = city.getLoot(seed, gen,false);
         for (ChestContent chest: loot)
-            if (contains(chest, item)) {
+            if (contains(chest)) {
                 itemLoc = structureLoc.toBlockPos();
                 return true;
             }
         return false;
     }
 
-    public static boolean contains(ChestContent chest, Item item) {
+    /**
+     * Checks if a given chest has the item
+     * If the item has no enchantments, default to the standard contains() method,
+     * otherwise, check if the item exists without enchants and then loop through the enchants
+     * @param chest the chest object to search for the item
+     * @return
+     */
+    public static boolean contains(ChestContent chest) {
         if (item.getEnchantments().size() == 0)
             return chest.contains(item);
         Pair<String, Integer> enchantment = item.getEnchantments().get(0);
--- src/main/java/com/and_penguin/WindowFrame.java
+++ src/main/java/com/and_penguin/WindowFrame.java
@@ -9,9 +9,18 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * The WindowFrame class controls the application window display and the user inputs
+ * into the text inputs, dropdown box, and button
+ *
+ * @author and_penguin
+ */
 public class WindowFrame {
-    public JFrame jFrame;
+    public JFrame jFrame; // This classes JFrame instance
 
+    /**
+     * Creates a JFrame instance and adds the various buttons and menus to it
+     */
     public WindowFrame() {
         jFrame = new JFrame();
         try {
@@ -28,46 +37,53 @@ public class WindowFrame {
         String[] enchantNames = Main.getEnchantNames();
 
 
-//        Font font = null;
-//        try {
-//            font = Font.createFont(Font.TRUETYPE_FONT, new File("MinecraftiaRegular.ttf"));
-//            GraphicsEnvironment ge =
-//                    GraphicsEnvironment.getLocalGraphicsEnvironment();
-//            ge.registerFont(font);
-//        } catch (IOException |FontFormatException e) {
-//            System.out.println(e);
-//        }
+        Font font = null;
+        try {
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("MinecraftiaRegular.ttf"));
+            GraphicsEnvironment ge =
+                    GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+        } catch (IOException |FontFormatException e) {
+            System.out.println(e);
+        }
 
-        JButton button = new JButton("Done");
+        JButton button = new JButton("Search");
         button.setBounds(160, 120, 90, 20);
-//        button.setFont(font.deriveFont(Font.BOLD, 12f));
+        button.setFont(font.deriveFont(Font.BOLD, 12f));
         JLabel output = new JLabel();
-        output.setBounds(60, 120, 500, 100);
-//        output.setFont(font.deriveFont(Font.BOLD, 12f));
+        output.setBounds(60, 150, 400, 100);
+        output.setFont(font.deriveFont(Font.BOLD, 18f));
         JLabel seedText = new JLabel();
         seedText.setBounds(20,20,80,20);
         seedText.setText("Seed:");
-//        seedText.setFont(font.deriveFont(Font.BOLD, 12f));
+        seedText.setFont(font.deriveFont(Font.BOLD, 12f));
         JTextField seedInput = new JTextField(20);
-        seedInput.setBounds(120, 20, 180, 20);
-//        seedInput.setFont(font.deriveFont(Font.BOLD, 12f));
+        seedInput.setBounds(120, 20, 220, 20);
+        seedInput.setFont(font.deriveFont(Font.BOLD, 12f));
         JTextField item = new JTextField(20);
-        item.setBounds(120,45,180,20);
-//        item.setFont(font.deriveFont(Font.BOLD, 12f));
+        item.setBounds(120,45,220,20);
+        item.setFont(font.deriveFont(Font.BOLD, 12f));
         JLabel itemText = new JLabel();
         itemText.setBounds(20,45,80,20);
         itemText.setText("Item Name:");
-//        itemText.setFont(font.deriveFont(Font.BOLD, 12f));
+        itemText.setFont(font.deriveFont(Font.BOLD, 12f));
         JComboBox<String> enchantments = new JComboBox<>(enchantNames);
         enchantments.setBounds(120, 70, 180, 20);
-//        enchantments.setFont(font.deriveFont(Font.BOLD, 12f));
+        enchantments.setFont(font.deriveFont(Font.BOLD, 12f));
         JTextField level = new JTextField(10);
         level.setBounds(300,70,40,20);
-//        level.setFont(font.deriveFont(Font.BOLD, 12f));
+        level.setFont(font.deriveFont(Font.BOLD, 12f));
         JLabel enchantText = new JLabel();
         enchantText.setBounds(20, 70, 100, 20);
         enchantText.setText("Enchant/lvl:");
-//        enchantText.setFont(font.deriveFont(Font.BOLD, 12f));
+        enchantText.setFont(font.deriveFont(Font.BOLD, 12f));
+        ImageIcon img = new ImageIcon("./images/logo.png");
+        JButton logo = new JButton();
+        logo.setBounds(375, 20, 70, 75);
+        logo.setIcon(img);
+        logo.setOpaque(false);
+        logo.setContentAreaFilled(false);
+        logo.setBorderPainted(false);
 
         jFrame.add(button);
         jFrame.add(output);
@@ -78,11 +94,13 @@ public class WindowFrame {
         jFrame.add(enchantments);
         jFrame.add(level);
         jFrame.add(enchantText);
+        jFrame.add(logo);
 
         jFrame.getContentPane().setBackground(new Color(212,212,212));
         jFrame.setTitle("Item Finder");
         jFrame.setLayout(null);
-        jFrame.setSize(400, 400);
+        jFrame.setResizable(false);
+        jFrame.setSize(500, 400);
 
         button.addActionListener(e -> {
             ArrayList<Pair<String, Integer>> enchants;
@@ -92,12 +110,16 @@ public class WindowFrame {
             else
                 enchants = Main.getEnchantByName(enchantName, Integer.parseInt(level.getText()));
             long seed = Long.parseLong(seedInput.getText());
-            Main.getItemByName(item.getText());
-            String location = Main.findItemOnSeed(seed, enchants);
-            output.setText("Seed: " + seed + " - Location: " + location);
+            if (Main.setItemByName(item.getText())) {
+                String location = Main.findItemOnSeed(seed, enchants);
+                output.setText("<html>" + location + "</html>");
+            }
         });
     }
 
+    /**
+     * Displays the window so the user can see it
+     */
     public void display() {
         jFrame.setVisible(true);
     }
